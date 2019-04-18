f3cceaab3f4e995d820d3289a88dd8b4acca55ad
Onno Broekmans <onnodb@gmail.com>
2019-04-18 17:52:54 +0200

    Fix default package imports

--- yamldiff/__init__.py
+++ yamldiff/__init__.py
@@ -2,3 +2,4 @@
 
 from .__about__ import (__author__, __copyright__, __doc__, __email__, __license__, __summary__,
                         __title__, __url__, __version__)
+from .yamldiff import DiffError, DiffContext, Diff, NodeType, YamlDiffer, pretty_print_diffs
